352714e31e7bebb58fb5c57d06ec0bb6ac4f4007
padahkim <padahkim@gmail.com>
2022-07-09 16:14:19 +0900

    devleop branch start

--- server/src/lib/checkLoggedIn.js
+++
@@ -1,9 +0,0 @@
-const checkLoggedIn = (req, res, next) => {
-  if (!req.user) {
-    res.status(401).send({ message: 'Unauthorized' }); //Unauthorized
-    return;
-  }
-  return next();
-};
-
-module.exports = checkLoggedIn;
