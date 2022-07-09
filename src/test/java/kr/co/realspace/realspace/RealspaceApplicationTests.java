eb265558c194154cb2ebcd37283f4de8cb5a39d5
padahkim <padahkim@gmail.com>
2022-07-09 22:26:36 +0900

    server setting

---
+++ src/test/java/kr/co/realspace/realspace/RealspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.realspace.realspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
